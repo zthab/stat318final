7b17d432cad77480735a6644fe1823019a8e46db
zthab <22862614+zthab@users.noreply.github.com>
2021-11-24 09:34:28 -0500

    screened out invalid responses

--- code/final_proj.R
+++ code/final_proj.R
@@ -1,7 +1,8 @@
 library(here)
+library(tidyverse)
 library(dplyr)
 #Load in data
-brfss_data <- read.csv(here('data','2015.csv'))
+brfss_data <- read.csv(here('data','2015.csv') )
 nehrs_data <- read.csv(here('data', 'NEHRS_2008-2017.csv'))
 fips_data <- read.csv(here('data', 'us-state-ansi-fips.csv'),
                       stringsAsFactors = F)
@@ -13,8 +14,15 @@ rm(fips_data)
 
 #excluding Guam, Puerto Rico from BRFSS data since 
 #is not present in NEHRS data 
-brfss_data <- brfss_data %>% rename(fips= X_STATE) %>%
-  filter(fips != 66 & fips != 72)
+brfss_data <- brfss_data %>% rename(fips= X_STATE) %>% filter((fips != 66 & fips != 72)) 
+#excluding survey observations made in 2015
+brfss_data <- brfss_data %>% mutate(IYEAR = str_extract(as.character(.$IYEAR), '[0-9]{4}')) %>% filter(IYEAR == '2015') 
+#excluding observations that were terminated early
+#because called condcuting landline survey and cell phone answere
+#or conducting cellphone survey and landline answered
+brfss_data <- filter(brfss_data, !((!is.na(CELLFON3)& CELLFON3 == 2)| (!is.na(CELLFON2) & CELLFON2 == 2)))
+#excluding observations that do not reside in the sate where they are answer they are.
+brfss_data <- filter(brfss_data, is.na(CSTATE) | CSTATE !=2)
 
 #when we are ready to merge both the nehrs and the BRFSS 
 full_data <- left_join(brfss_data, nehrs_data, by = 'fips')
