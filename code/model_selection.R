1f8c7d1a2ca85ca07be0cebf09bbb4006165d1c2
czhao0526 <86986732+czhao0526@users.noreply.github.com>
2021-12-02 22:30:16 -0500

    added testing and training sections

--- code/model_selection.R
+++ code/model_selection.R
@@ -1,17 +1,78 @@
 library(car)
+library(dplyr)
 load("full_comp_1.rda")
 colnames(full_data)
+
+# creates training and testing data (divided into 2/3 and 1/3)
+set.seed(123)
+indicesTrainingSet<-sample(nrow(full_data), length(full_data$X_RFHLTH)*2/3, replace=FALSE)
+train<-full_data[indicesTrainingSet,]
+test<-full_data[-indicesTrainingSet,]
+
+testing <- s
 # Included any variable with more than 5 levels
-model.logit <- glm(X_RFHLTH~NUMADULT + MARITAL + EDUCA + EMPLOY1 + CHILDREN + INCOME2 + X_BMI5 + SCNTWRK1 + X_PRACE1 +  pct_phys_any_ehr + pct_phys_basic_ehr + pct_phys_cert_ehr + pct_primary_care_phys_cert_ehr + pct_surg_med_spec_phys_cert_ehr +  pct_small_practice_phys_cert_ehr + pct_phys_send_receive_any_patient_info + pct_phys_e_share_patients + pct_phys_patient_secure_message + pct_phys_vdt +pct_phys_vd_and_t + pct_phys_find_clin_info + pct_phys_send_any_clin_info + pct_phys_send_summary_care_record + pct_phys_receive_any_clin_info + pct_phys_receive_summary_care_record + pct_phys_integrate_any_clin_info + pct_phys_integrate_summary_care_record,family=binomial(link="logit"), data = full_data)
+model.logit <- glm(X_RFHLTH~NUMADULT + MARITAL + 
+                     EDUCA + EMPLOY1 + CHILDREN + INCOME2 + 
+                     X_BMI5 + SCNTWRK1 + X_PRACE1 +  pct_phys_any_ehr + 
+                     pct_phys_basic_ehr + pct_phys_cert_ehr + pct_primary_care_phys_cert_ehr +
+                     pct_surg_med_spec_phys_cert_ehr +  pct_small_practice_phys_cert_ehr + 
+                     pct_phys_send_receive_any_patient_info + pct_phys_e_share_patients +
+                     pct_phys_patient_secure_message + pct_phys_vdt +pct_phys_vd_and_t + 
+                     pct_phys_find_clin_info + pct_phys_send_any_clin_info + 
+                     pct_phys_send_summary_care_record + pct_phys_receive_any_clin_info + 
+                     pct_phys_receive_summary_care_record + pct_phys_integrate_any_clin_info + 
+                     pct_phys_integrate_summary_care_record,
+                   family=binomial(link="logit"), data = train)
 vif(model.logit) # pct_phys_cert_ehr highest
-model.logit <- glm(X_RFHLTH~NUMADULT + MARITAL + EDUCA + EMPLOY1 + CHILDREN + INCOME2 + X_BMI5 + SCNTWRK1 + X_PRACE1 +  pct_phys_any_ehr + pct_phys_basic_ehr + pct_primary_care_phys_cert_ehr + pct_surg_med_spec_phys_cert_ehr +  pct_small_practice_phys_cert_ehr + pct_phys_send_receive_any_patient_info + pct_phys_e_share_patients + pct_phys_patient_secure_message + pct_phys_vdt +pct_phys_vd_and_t + pct_phys_find_clin_info + pct_phys_send_any_clin_info + pct_phys_send_summary_care_record + pct_phys_receive_any_clin_info + pct_phys_receive_summary_care_record + pct_phys_integrate_any_clin_info + pct_phys_integrate_summary_care_record,family=binomial(link="logit"), data = full_data)
+model.logit <- glm(X_RFHLTH~NUMADULT + MARITAL + 
+                     EDUCA + EMPLOY1 + CHILDREN + INCOME2 + 
+                     X_BMI5 + SCNTWRK1 + X_PRACE1 +  pct_phys_any_ehr +
+                     pct_phys_basic_ehr + pct_primary_care_phys_cert_ehr + 
+                     pct_surg_med_spec_phys_cert_ehr +  pct_small_practice_phys_cert_ehr + 
+                     pct_phys_send_receive_any_patient_info + pct_phys_e_share_patients + 
+                     pct_phys_patient_secure_message + pct_phys_vdt +pct_phys_vd_and_t +
+                     pct_phys_find_clin_info + pct_phys_send_any_clin_info + 
+                     pct_phys_send_summary_care_record + pct_phys_receive_any_clin_info + 
+                     pct_phys_receive_summary_care_record + pct_phys_integrate_any_clin_info + 
+                     pct_phys_integrate_summary_care_record,
+                   family=binomial(link="logit"), data = train)
 vif(model.logit) # ct_phys_send_receive_any_patient_info highest
-model.logit <- glm(X_RFHLTH~NUMADULT + MARITAL + EDUCA + EMPLOY1 + CHILDREN + INCOME2 + X_BMI5 + SCNTWRK1 + X_PRACE1 +  pct_phys_any_ehr + pct_phys_basic_ehr + pct_primary_care_phys_cert_ehr + pct_surg_med_spec_phys_cert_ehr +  pct_small_practice_phys_cert_ehr + pct_phys_e_share_patients + pct_phys_patient_secure_message + pct_phys_vdt +pct_phys_vd_and_t + pct_phys_find_clin_info + pct_phys_send_any_clin_info + pct_phys_send_summary_care_record + pct_phys_receive_any_clin_info + pct_phys_receive_summary_care_record + pct_phys_integrate_any_clin_info + pct_phys_integrate_summary_care_record,family=binomial(link="logit"), data = full_data)
+model.logit <- glm(X_RFHLTH~NUMADULT + MARITAL + 
+                     EDUCA + EMPLOY1 + CHILDREN + INCOME2 + 
+                     X_BMI5 + SCNTWRK1 + X_PRACE1 + pct_phys_any_ehr + 
+                     pct_phys_basic_ehr + pct_primary_care_phys_cert_ehr + 
+                     pct_surg_med_spec_phys_cert_ehr +  pct_small_practice_phys_cert_ehr + 
+                     pct_phys_e_share_patients + pct_phys_patient_secure_message + 
+                     pct_phys_vdt +pct_phys_vd_and_t + pct_phys_find_clin_info + 
+                     pct_phys_send_any_clin_info + pct_phys_send_summary_care_record + 
+                     pct_phys_receive_any_clin_info + pct_phys_receive_summary_care_record + 
+                     pct_phys_integrate_any_clin_info + pct_phys_integrate_summary_care_record,
+                   family=binomial(link="logit"), data = train)
 vif(model.logit) # EMPLOY1 highest
-model.logit <- glm(X_RFHLTH~NUMADULT + MARITAL + EDUCA + CHILDREN + INCOME2 + X_BMI5 + SCNTWRK1 + X_PRACE1 +  pct_phys_any_ehr + pct_phys_basic_ehr + pct_primary_care_phys_cert_ehr + pct_surg_med_spec_phys_cert_ehr +  pct_small_practice_phys_cert_ehr + pct_phys_e_share_patients + pct_phys_patient_secure_message + pct_phys_vdt +pct_phys_vd_and_t + pct_phys_find_clin_info + pct_phys_send_any_clin_info + pct_phys_send_summary_care_record + pct_phys_receive_any_clin_info + pct_phys_receive_summary_care_record + pct_phys_integrate_any_clin_info + pct_phys_integrate_summary_care_record,family=binomial(link="logit"), data = full_data)
+model.logit <- glm(X_RFHLTH~NUMADULT + MARITAL + 
+                     EDUCA + CHILDREN + INCOME2 + X_BMI5 + 
+                     SCNTWRK1 + X_PRACE1 +  pct_phys_any_ehr + 
+                     pct_phys_basic_ehr + pct_primary_care_phys_cert_ehr + 
+                     pct_surg_med_spec_phys_cert_ehr + pct_small_practice_phys_cert_ehr + 
+                     pct_phys_e_share_patients + pct_phys_patient_secure_message + 
+                     pct_phys_vdt +pct_phys_vd_and_t + pct_phys_find_clin_info + 
+                     pct_phys_send_any_clin_info + pct_phys_send_summary_care_record + 
+                     pct_phys_receive_any_clin_info + pct_phys_receive_summary_care_record + 
+                     pct_phys_integrate_any_clin_info + pct_phys_integrate_summary_care_record,
+                   family=binomial(link="logit"), data = train)
 vif(model.logit) # pct_phys_e_share_patients highest
-model.logit <- glm(X_RFHLTH~NUMADULT + MARITAL + EDUCA + CHILDREN + INCOME2 + X_BMI5 + SCNTWRK1 + X_PRACE1 +  pct_phys_any_ehr + pct_phys_basic_ehr + pct_primary_care_phys_cert_ehr + pct_surg_med_spec_phys_cert_ehr +  pct_small_practice_phys_cert_ehr + pct_phys_patient_secure_message + pct_phys_vdt +pct_phys_vd_and_t + pct_phys_find_clin_info + pct_phys_send_any_clin_info + pct_phys_send_summary_care_record + pct_phys_receive_any_clin_info + pct_phys_receive_summary_care_record + pct_phys_integrate_any_clin_info + pct_phys_integrate_summary_care_record,family=binomial(link="logit"), data = full_data)
-vif(model.logit) # none others are higher than 10 (next highest is pct_phys_patient_secure_message at 7.3)
+model.logit <- glm(X_RFHLTH~NUMADULT + MARITAL + 
+                     EDUCA + CHILDREN + INCOME2 + X_BMI5 + 
+                     SCNTWRK1 + X_PRACE1 +  pct_phys_any_ehr + 
+                     pct_phys_basic_ehr + pct_primary_care_phys_cert_ehr + 
+                     pct_surg_med_spec_phys_cert_ehr +  pct_small_practice_phys_cert_ehr + 
+                     pct_phys_patient_secure_message + pct_phys_vdt +pct_phys_vd_and_t + 
+                     pct_phys_find_clin_info + pct_phys_send_any_clin_info + 
+                     pct_phys_send_summary_care_record + pct_phys_receive_any_clin_info + 
+                     pct_phys_receive_summary_care_record + pct_phys_integrate_any_clin_info + 
+                     pct_phys_integrate_summary_care_record,
+                   family=binomial(link="logit"), data = train)
+vif(model.logit) # none other are higher than 8 (next highest is pct_phys_receive_summary_care_record at 7.7)
 
 final_hlth <- glm(X_RFHLTH ~ NUMADULT + PVTRESD1 + SEX + MARITAL + EDUCA + RENTHOM1 + VETERAN3 + CHILDREN + INCOME2 + X_BMI5 + PREGNANT + SCNTWRK1 + X_PRACE1 + X_HISPANC + HLTHPLN1 + INTERNET + EXERANY2 + X_SMOKER3 + pct_phys_any_ehr + pct_phys_basic_ehr + pct_primary_care_phys_cert_ehr + pct_surg_med_spec_phys_cert_ehr +  pct_small_practice_phys_cert_ehr + pct_phys_patient_secure_message + pct_phys_vdt +pct_phys_vd_and_t + pct_phys_find_clin_info + pct_phys_send_any_clin_info + pct_phys_send_summary_care_record + pct_phys_receive_any_clin_info + pct_phys_receive_summary_care_record + pct_phys_integrate_any_clin_info + pct_phys_integrate_summary_care_record,family=binomial(link="logit"), data = full_data)
 final_checkup <- glm(CHECKUP1CLEAN ~ NUMADULT + PVTRESD1 + SEX + MARITAL + EDUCA + RENTHOM1 + VETERAN3 + CHILDREN + INCOME2 + X_BMI5 + PREGNANT + SCNTWRK1 + X_PRACE1 + X_HISPANC + HLTHPLN1 + INTERNET + EXERANY2 + X_SMOKER3 + pct_phys_any_ehr + pct_phys_basic_ehr + pct_primary_care_phys_cert_ehr + pct_surg_med_spec_phys_cert_ehr +  pct_small_practice_phys_cert_ehr + pct_phys_patient_secure_message + pct_phys_vdt +pct_phys_vd_and_t + pct_phys_find_clin_info + pct_phys_send_any_clin_info + pct_phys_send_summary_care_record + pct_phys_receive_any_clin_info + pct_phys_receive_summary_care_record + pct_phys_integrate_any_clin_info + pct_phys_integrate_summary_care_record,family=binomial(link="logit"), data = full_data)
